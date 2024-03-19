6b67690c1d5748ed0824b1ab8a2ae08c605c5fc2
San88Francisco <136508377+San88Francisco@users.noreply.github.com>
2024-03-19 19:27:21 +0100

    go

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,18 @@ import './App.css'
 
 function App() {
 
-  return (
-    <>
-      <h1>Team work</h1>
-      <h1>Goncharuk Nikita</h1>
-      <h4>Barnabishvili Robert</h4>
-      <h4>Vasyl Verbytskiy</h4>
-      <h2>Xlussov</h2>
-      <h4>Yurii Stepaniuk</h4>
-      <h4>Iryna Khylchuk</h4>
+   return (
+      <>
+         <h1>Team work</h1>
+         <h1>Goncharuk Nikita</h1>
+         <h4>Barnabishvili Robert</h4>
+         <h4>Vasyl Verbytskiy</h4>
+         <h2>Xlussov</h2>
+         <h4>Yurii Stepaniuk</h4>
+         <h4>Iryna Khylchuk</h4>
+         <h1>The best team</h1>
       </>
    )
-  
 }
 
 export default App
