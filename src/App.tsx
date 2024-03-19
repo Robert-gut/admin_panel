df9f6625f6aabe9bfd1baba937cc3afd2b27d423
Robert-gut <71551473+Robert-gut@users.noreply.github.com>
2024-03-19 20:26:15 +0200

    added !!!

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ function App() {
   return (
     <>
       <h1>Team work</h1>
-      <h1>Goncharuk Nikita</h1>
-      <h4>Barnabishvili Robert</h4>
+      <h4>Goncharuk Nikita</h4>
+      <h4>Barnabishvili Robert!!!!!!!!!</h4>
       <h4>Vasyl Verbytskiy</h4>
-      <h2>Xlussov</h2>
+      <h4>Xlussov</h4>
       <h4>Yurii Stepaniuk</h4>
       <h4>Iryna Khylchuk</h4>
       </>
