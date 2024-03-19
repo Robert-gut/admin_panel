b941e46199006568aa53718768a472f3eb96993f
Xlussov <130926999+Xlussov@users.noreply.github.com>
2024-03-19 20:21:54 +0200

    second

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,11 @@ function App() {
       <h1>Goncharuk Nikita</h1>
       <h4>Barnabishvili Robert</h4>
       <h4>Vasyl Verbytskiy</h4>
-      <h2>Xlussov</h2>
+      <h2>Xlussov - Front-end developer</h2>
       <h4>Yurii Stepaniuk</h4>
       <h4>Iryna Khylchuk</h4>
+
+
       </>
    )
   
