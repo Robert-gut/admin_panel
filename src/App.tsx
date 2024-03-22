16d8a6a3b03d18c1de8c493163b8f890c50f843d
Robert-gut <71551473+Robert-gut@users.noreply.github.com>
2024-03-22 18:47:38 +0200

    added react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Admin from './pages/Admin/Admin'
 
 function App() {
-
    return (
-      <>
-         <h1>Team work</h1>
-         <h1>Goncharuk Nikita</h1>
-         <h4>Barnabishvili Robert!!!!!!!!!</h4>
-         <h4>Vasyl Verbytskiy</h4>
-         <h2>Xlussov - Front-end developer</h2>
-         <h4>Yurii Stepaniuk</h4>
-         <h4>Iryna Khylchuk!</h4>
-         <h1>The best team</h1>
-      </>
+      <Router>
+        <Routes>
+          <Route path='/admin' element={<Admin />}/>
+          <Route path='/logIn' element={<Admin />}/>//! log in
+          <Route path='*' element={<Admin />}/>//! not found
+        </Routes>
+      </Router>
    )
 }
 
