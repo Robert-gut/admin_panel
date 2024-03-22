784b6fb611dc99686e719483e98826ed0b5d86b1
Robert-gut <71551473+Robert-gut@users.noreply.github.com>
2024-03-22 19:42:46 +0200

    added sidebar theend

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -1,12 +1,19 @@
-import Sidebar from "../../components/Sidebar/Sidebar"
+import { Box, Grid } from "@mui/material";
+import Sidebar from "../../components/Sidebar/Sidebar";
 
-function Admin(){
+function Admin() {
   return (
-    <>
-    <h1>Admin</h1>
-    <Sidebar/>
-    </>
-  )
+    <Box sx={{flexGrow: 1}}>
+      <Grid container spacing={2}>
+        <Grid item xs={3}>
+          <Sidebar />
+        </Grid>
+        <Grid item xs={9}>
+          <h1>Admin</h1>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
 
-export default Admin
+export default Admin;
